4e41af293954f5cc52bc2965ae032ea650ac09a1
Justin Carlson <manfrompabb@gmail.com>
2024-10-03 13:12:35 -0700

    optimization

--- tutorial2/tutorial2.cpp
+++ tutorial2/tutorial2.cpp
@@ -11,52 +11,48 @@ namespace filter {
                                          {0, 0, 0},
                                          {-1, -2, -1}};
 
-    void apply_greyscale(cv::Mat image) {
+    cv::Mat apply_greyscale(cv::Mat image) {
+        cv::Mat grey_image = cv::Mat::zeros(image.size(), CV_8U);
         unsigned char *rgb = (unsigned char *) image.data;
         int channels = image.channels();
         for (int i = 0; i < image.rows; i++) {
             for (int j = 0; j < image.cols; j++) {
                 int index = i * image.step + j * channels; // calculate data offset from current position/channel
 
-                char b = rgb[index];
-                char g = rgb[index + 1];
-                char r = rgb[index + 2];
+                unsigned char b = rgb[index];
+                unsigned char g = rgb[index + 1];
+                unsigned char r = rgb[index + 2];
 
-                char grey = greyscale(r, g, b); // calculate the grey value
+                unsigned char grey = greyscale(r, g, b); // calculate the grey value
 
-                rgb[index] = grey;
-                rgb[index + 1] = grey;
-                rgb[index + 2] = grey;
+                grey_image.ptr<uchar>(i)[j] = grey;
             }
         }
+        return grey_image;
     }
 
     cv::Mat apply_convolution(cv::Mat image) {
-        cv::Mat sobel = cv::Mat::zeros(image.size(), CV_16SC3); // initialize a 3-channel matrix with size equal to the image represented by signed shorts
-
+        cv::Mat sobel = cv::Mat::zeros(image.size(), CV_8U); // initialize the output matrix as a 8U matrix
         for (int i = 1; i < image.rows - 1; i++) {
             for (int j = 1; j < image.cols - 1; j++) {
 
-                short gradX = 0, gradY = 0, gradMag;
+                short gradX = 0, gradY = 0, gradMag; // initialize intermediate calc values as shorts to prevent overflow
 
                 for (int ki = -1; ki <= 1; ki++) {
                     for (int kj = -1; kj <= 1; kj++) {
-                        BGR &anchor = image.ptr<BGR>(i + ki)[j + kj]; // get BGR value for the pixel position + the convolution offset
-                        gradX += anchor.B * filter::Gx[ki + 1][kj + 1]; // calculate the X gradient from the X kernel
-                        gradY += anchor.B * filter::Gy[ki + 1][kj + 1]; // calculate the Y gradient from the Y kernel
+                        uchar anchor = image.ptr<uchar>(i + ki)[j + kj]; // get BGR value for the pixel position + the convolution offset
+                        gradX += anchor * filter::Gx[ki + 1][kj + 1]; // calculate the X gradient from the X kernel
+                        gradY += anchor * filter::Gy[ki + 1][kj + 1]; // calculate the Y gradient from the Y kernel
                     }
                 }
 
                 gradMag = abs(gradX) + abs(gradY); // approximate gradient magnitude from absolute value of component sums
 
-                BGRS sobel_color = {gradMag, gradMag, gradMag}; // create the struct to store pixel BGR value as a short
+                if (gradMag > 255) gradMag = 255;
 
-                sobel.ptr<BGRS>(i)[j] = sobel_color; // set BGR elements of pixel to corresponding filtered value
+                sobel.ptr<uchar>(i)[j] = gradMag; // set BGR elements of pixel to corresponding filtered value
             }
         }
-
-        normalize(sobel, sobel, 0, 255, cv::NORM_MINMAX, CV_8UC3); // convert each RGB element from a short to unsigned int to display properly
-
         return sobel;
     }
 
@@ -73,7 +69,7 @@ int main(int argc, char *argv[]) {
     }
 
     cv::VideoCapture capture(argv[1]);
-    cv::Mat image, sobel;
+    cv::Mat image, greyscale, sobel;
 
     if (!capture.isOpened()) {
         throw std::runtime_error("file cannot be opened...\n");
@@ -85,8 +81,8 @@ int main(int argc, char *argv[]) {
     while (true) {
         capture >> image; // get next image from video stream
         if (image.empty()) break; // exit if no more frames
-        filter::apply_greyscale(image); // apply greyscaling to the current image in-place
-        sobel = filter::apply_convolution(image); // apply the Sobel filter to the given frame
+        greyscale = filter::apply_greyscale(image); // apply greyscaling to the current image in-place
+        sobel = filter::apply_convolution(greyscale); // apply the Sobel filter to the given frame
         framesCreated++; // increment frame counter
         printf("%d/%d frames computed...\n", framesCreated, (int) capture.get(cv::CAP_PROP_FRAME_COUNT));
         cv::imshow(argv[1], sobel); // display the image
--- tutorial2/tutorial2.hpp
+++ tutorial2/tutorial2.hpp
@@ -5,15 +5,3 @@
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
 #include <cmath>
-
-typedef struct BGR {
-    uchar B;
-    uchar G;
-    uchar R;
-} BGR;
-
-typedef struct BGRS {
-    short B;
-    short G;
-    short R;
-} BGRS;
